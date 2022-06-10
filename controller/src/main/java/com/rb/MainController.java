c68f73cd624e0b4a65fdb53041af24ae1809f636
Richard Beneš <richard.benes@monster.com>
2022-06-10 12:10:14 +0200

    Add sources

---
+++ controller/src/main/java/com/rb/MainController.java
@@ -0,0 +1,18 @@
+package com.rb;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public ResponseEntity<String> func() {
+        return ResponseEntity.ok("Hello!");
+    }
+
+}
+
